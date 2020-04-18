2839f815e7b22cd37396973b0d366c043211a9cd
Javier Rial <JavierRial89@gmail.com>
2020-04-18 18:30:04 +0200

    Se traslada el código JavaScript a un fichero.
    Se traslada el código JavaScript a un fichero externo alojado en la carpeta JS para limpiar un poco el contenido.

--- index.php
+++ index.php
@@ -25,49 +25,6 @@ require_once("./class/Conexion.php");
         require_once("./includes/home.php");
         require_once("./includes/footer.php");
     ?>
-    <script>
-        $(function(){
-
-            $(".copy").click(function(){
-                var copyText = document.getElementById("recibida");
-                copyText.select();
-                copyText.setSelectionRange(0, 99999)
-                document.execCommand("copy");
-                 $(".mensaje").slideDown( "fast", function() { });
-            })
-            $("form#acorta").submit(function(evt){
-                evt.preventDefault();
-                var url = $("input[name=url]").val(); 
-                var msg = true;
-                if(url.length < 0){
-                    msg = "El campo no puede ir vacío";
-                }
-		        var parametros = {
-			        "url" : url
-		        };
-                if(msg != ''){
-                    $.ajax({
-                        data: parametros, 
-                        url:   '/acortador/process/valida.php',
-                        type:  'POST', 
-                        dataType : 'json',
-                        beforeSend: function(){
-                            // Antes de enviar..
-                        },
-                        success: function(data){
-                            // enviado
-                            if(data.success){
-                                $("input[name=recibida]").val("http://localhost/acortador/"+data.short);
-                            $( "#resultado" ).slideDown( "slow", function() {
-                              });
-                            }else{
-                                $("#formurl").effect("bounce", {times:3}, 300);
-                            }
-    	                }
-                    });
-                }
-            })
-        })
-    </script>
+    <script type="text/javascript" src="/acortador/js/functions.js">
 </body>
 </html>
